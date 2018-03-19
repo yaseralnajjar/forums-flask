ced2d5e29740ecff282fc3c6e4e9c4db1163dcc1
0xRumple <kasseryasser@windowslive.com>
2018-03-19 14:22:14 +0200

    Merge branch 'release/1.3.0'

--- app/__init__.py
+++ app/__init__.py
@@ -14,7 +14,7 @@ from app import stores, dummy_data
 
 member_store = stores.MemberStore()
 post_store = stores.PostStore()
-dummy_data.seed_stores(member_store, post_store)
+#dummy_data.seed_stores(member_store, post_store)
 
 from app import views
 from app import api
